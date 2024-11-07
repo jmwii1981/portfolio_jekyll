5f9006c6084fca4baff4c47b9271f341d3447dac
Jan Michael Wallace <hello@janmichael.io>
2024-11-07 15:24:49 -0500

    cleans up scripts folder from old project, adds new script that retrieves latest post from my medium blog, this push will publish my mvp release

--- scripts/factories/addToDocument.mjs
+++
@@ -1,5 +0,0 @@
-const addToDocumentFactory = function(elementId, thingToAdd) {
-    elementId = document.getElementById(elementId);
-    thingToAdd.forEach(thing => elementId.append(thing));
-}
-export { addToDocumentFactory };
